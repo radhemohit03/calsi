4e172784d014a50e9f88d200b9dabb6666559719
Mohit Thukrele <mohitthukrele2003@gmail.com>
2024-01-18 14:55:25 +0530

    hello

---
+++ Official/new.js
@@ -0,0 +1,88 @@
+
+/////////       FUNCTION
+
+// function fn(a,b){
+//     console.log(a,b)
+// }
+// fn(10,30)
+
+// const fn=(a,b)=>{
+//     console.log(a,b)
+// }
+// fn(100,200)
+
+///////          object
+
+// let obj={
+//     name:'Mohit',
+//     age:20,
+//     gender:'M',
+//     isPass:false,
+// }
+// obj.age
+
+// console.log("Mohit" + " " + "Thukrele")
+
+// let firstName="Anshu"
+// let lastName="Sahu"
+
+// console.log(`${firstName}${lastName}`)
+
+// function fullName(anythink){
+//     console.log(`${anythink.name1}  ${anythink.lastName}`)
+
+// }
+
+// let obj={
+//     name1:'Mohit',
+//     lastName:'Thukrele',
+// }
+
+// fullName(obj)
+
+/////////////ARR
+
+// let arr=['mohit', 'LOve', 'Gini', 143,true]
+// console.log(arr[2])
+
+
+// let div= document.querySelector('div')
+//  let h1= document.createElement('h1')
+//  h1.innerText='Hello'
+//  div.appendChild(h1)
+
+//  console.log(div)
+
+// let div= document.querySelector('div')
+// div.remove()
+
+//////   EVENT
+//one
+// function fun1(){
+//     console.log('OfficialRadhe')
+// }
+//two
+// let btn= document.querySelector('button')
+// btn.onclick=function(){
+//     console.log('mohit')
+// }
+//three
+// let btn= document.querySelector('button')
+// btn=addEventListener('click',function(){
+//     console.log('KingBaba')
+// })
+//SECOND TYPE OF EVENT
+
+// let inp= document.querySelector('button')
+// inp=addEventListener('input',function(){
+//     console.log('KingBaba')
+// })
+
+// let inp= document.querySelector('input')
+// inp=addEventListener('input',function(e){
+//     console.log(e.target.value)
+// })
+
+
+
+
